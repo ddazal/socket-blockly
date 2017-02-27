05766efe52ba54c386e27caaa84cad5b1ffab4a1
David Daza <03dazal@gmail.com>
2017-02-27 07:50:15 -0500

    Basic setup for socket.io

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
 const port = process.env.PORT || 3000
+
+var io = socketio(server)
+
+io.on('connection', (socket) => {
+	console.log(`User ${socket.id} connected`)
+})
 
 app.use(express.static(__dirname + '/public'))
 
