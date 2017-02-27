59030984c9c748cb57b31f191df79de597837619
David Daza <03dazal@gmail.com>
2017-02-27 07:27:11 -0500

    Blockly inject

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const app = express()
 const server = http.createServer(app)
 const port = process.env.PORT || 3000
 
+app.use(express.static(__dirname + '/public'))
+
 server.listen(port, () => console.log(`http://localhost:${port}/`))
---
+++ public/main.js
@@ -0,0 +1,9 @@
+var workspaceData = {
+	grid: {
+		spacing: 10
+	},
+	toolbox: document.getElementById('toolbox'),
+	trashcan: true
+}
+
+var workspace = Blockly.inject('blocklyDiv', workspaceData)
